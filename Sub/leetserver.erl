08e71a17471810e4eabe248c55f5243819133db8
Melburn <Melburn1@hotmail.com>
2012-09-27 12:09:56 +0200

    fixat leetserver

---
+++ Sub/leetserver.erl
@@ -0,0 +1,62 @@
+-module(leetserver).
+-compile(export_all).
+
+start() ->
+	try
+		Pid = spawn(?MODULE, loop, []),
+		register(gsserver, Pid)
+	catch
+		_:_ -> error_start
+	end.
+
+stop() ->
+	Ref = monitor(process, gsserver),
+	try
+		gsserver ! stop,
+		receive
+			{'DOWN', Ref, process, {gsserver, _},_} ->
+				{ok, stopped}
+		after 3000 ->
+				exit(whereis(gsserver), kill),
+				receive
+					{'DOWN', Ref, process, _, _} ->
+						{ok, killed}
+				after 3000 ->
+						demonitor(Ref, [flush]),
+						{error, timeout}
+				end
+		end		
+	catch
+	_:_ ->
+		demonitor(Ref, [flush]),
+		{error, timeout}
+	end.
+
+loop() ->
+	ImgPid = imgserver:start(),
+	DocPid = docserver:start(),
+	DBPid = dbserver:start(),
+	loop(ImgPid, DocPid, DBPid).
+
+loop(ImgPid, DocPid, DBPid) -> 
+receive
+	{Pid, Ref, Tag, Data} ->
+		case Tag of
+			img -> imgcall(ImgPid, {Pid, Ref, get, Data});
+			doc -> doccall(DocPid, {Pid, Ref, get, Data});
+			dbquery -> dbcall(DBPid, {Pid, Ref, get, Data})
+		end,
+		loop(ImgPid, DocPid, DBPid)
+end.
+
+imgcall(ImgPid, {Pid, Ref, Tag, {Imgname, W, H}}) ->
+	ActorPid = leetactor:start(),
+	ActorPid ! {Pid, Ref, Tag, {Imgname, W, H}, ImgPid}.
+	
+doccall(DocPid, {Pid, Ref, Tag, Data}) ->
+	ActorPid = leetactor:start(),
+	ActorPid ! {Pid, Ref, Tag, Data, DocPid}.
+
+dbcall(DBPid, {Pid, Ref, Tag, Data}) ->
+	ActorPid = leetactor:start(),
+	ActorPid ! {Pid, Ref, Tag, Data, DBPid}.
